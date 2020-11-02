d0d93c2b638558e1943e70b4c8df8bad02df1d8e
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2020-11-02 12:40:25 +0530

    build demos

---
+++ docker-demo5/db/data.sql
@@ -0,0 +1,9 @@
+CREATE table employees
+(
+    id int primary key,
+    ename varchar(20),
+    desg varchar(20)
+);
+
+INSERT into employees VALUES(101, 'Guddu','Manager');
+INSERT into employees VALUES(102, 'Golu','Asst. Manager');
